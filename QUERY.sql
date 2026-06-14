b0e919baecd2c6d9c64bbb9fc84abe1255ae23f2
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-06-14 20:32:26 +0600

    query 4

--- QUERY.sql
+++ QUERY.sql
@@ -119,3 +119,14 @@ SELECT
     COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM bookings
 WHERE payment_status IS NULL;
+
+
+-- QUERY 4:
+SELECT 
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN matches m ON b.match_id = m.match_id;
